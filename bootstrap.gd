6724a6e6b11d827f4f29eb242cfb5ff5f160778a
Yvonnick FRIN <frin.yvonnick@gmail.com>
2025-09-26 15:22:30 +0200

    Add code to bootstrap lobby server

---
+++ bootstrap.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@onready var lobby_server: LobbyServer = %LobbyServer
+
+func _ready():
+	lobby_server._paths = Config.arguments.get("paths", {})
+	lobby_server._log_folder = Config.arguments.get("log_folder", "")
+	lobby_server._environment = Config.arguments.get("environment", "development")
+	lobby_server.start(Config.arguments.get("port", 17018))
