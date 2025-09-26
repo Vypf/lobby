4866f5fb3a4ec5a6ea6113fb1a69157144879d42
Yvonnick FRIN <frin.yvonnick@gmail.com>
2025-09-26 16:41:31 +0200

    Update godot_multiplayer and add executable paths

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # lobby
+
+Example of args to pass:
+
+--environment=development --log_folder=C:\Users\something\lobby\logs --executable_paths survivors="C:\Users\something\Godot_v4.4-stable_win64.exe\Godot_v4.4-stable_win64.exe" --paths survivors=C:\Users\something\survivors-game
--- bootstrap.gd
+++ bootstrap.gd
@@ -4,6 +4,7 @@ extends Node
 
 func _ready():
 	lobby_server._paths = Config.arguments.get("paths", {})
+	lobby_server._executable_paths = Config.arguments.get("executable_paths", {})
 	lobby_server._log_folder = Config.arguments.get("log_folder", "")
 	lobby_server._environment = Config.arguments.get("environment", "development")
 	lobby_server.start(Config.arguments.get("port", 17018))
